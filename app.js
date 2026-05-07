01616d82c3e714efb956a8a613f09541bd9ba797
ashwani <vicky.nick1991@gmail.com>
2026-05-07 09:49:18 +0530

    first backend app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('<h1>App Page</h1>');
+});
+
+// Hello route
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello Page</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
